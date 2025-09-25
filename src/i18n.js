85df36e7919872abd514da004ef88e5cd1b2161c
andre <andrepv2003@gmail.com>
2025-09-25 16:09:42 -0500

    feat(i18n): add internationalization support for english and spanish

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,12 @@
 import {createI18n} from "vue-i18n";
 import en from "./locales/en.json";
-import zh from "./locales/es.json";
+import es from "./locales/es.json";
 
 const i18n = createI18n({
-    legacy:false,
+    legacy: false,
     locale: 'en',
     fallbackLocale: 'en',
-    messages: {en,es}
+    messages: { en, es}
 });
+
 export default i18n;
